aee1ca599c07138a32e5e1309dca633b49521b55
Fred Oliveira <fred@webreakstuff.com>
2012-06-04 18:40:03 +0100

    Moved things around. Made the code more ruby-friendly

--- autoingestion.rb
+++ autoingestion.rb
@@ -3,42 +3,7 @@ require 'net/https'
 require 'uri'
 require 'cgi'
 
-def main
-  autoingestor = Autoingestion.new
-  yesterday = (Time.new - 24*60*60).strftime("%Y%m%d")
-
-  if File.exists?("config.hash")
-    config = eval(File.new("config.hash").read())
-    autoingestor.username = config["username"]
-    autoingestor.password = config["password"]
-    autoingestor.vendor_number = config["vendor_number"]
-  else
-    print "iTunes Connect email address:             "
-    autoingestor.username = gets.strip!
-    print "iTunes Connect password:                  "
-    autoingestor.password = gets.strip!
-    print "Vendor number:                            "
-    autoingestor.vendor_number = gets.strip!
-    print "Type of report:                [Sales]    "
-    autoingestor.type_of_report = gets.strip!
-    print "Date type:                     [Daily]    "
-    autoingestor.date_type = gets.strip!
-    print "Report type:                   [Summary]  "
-    autoingestor.report_type = gets.strip!
-    print "Report date:                   [#{yesterday}] "
-    autoingestor.report_date = gets.strip!
-  end
-
-  autoingestor.type_of_report = "Sales"   if autoingestor.type_of_report.nil?
-  autoingestor.date_type      = "Daily"   if autoingestor.date_type.nil?
-  autoingestor.report_type    = "Summary" if autoingestor.report_type.nil?
-  autoingestor.report_date    = yesterday if autoingestor.report_date.nil?
-
-  autoingestor.perform_request
-end
-
 class Autoingestion
-
   ITUNES_URL = "https://reportingitc.apple.com/autoingestion.tft"
 
   attr_accessor :username
@@ -61,7 +26,6 @@ class Autoingestion
 
   def perform_request
     uri = URI.parse(ITUNES_URL)
-    
     request = Net::HTTP.new(uri.host, uri.port)
     request.use_ssl = true
     
@@ -75,11 +39,6 @@ class Autoingestion
       f.write(response.body)
       f.close
       puts "File Downloaded Successfully (#{filename})"
-
-      system("cp #{filename} #{filename}.bak")
-      system("gunzip #{filename}")
-      system("mv #{filename}.bak #{filename}")
-      
     elsif response['errormsg'] != nil
       puts response['errormsg']
     else
@@ -89,9 +48,35 @@ class Autoingestion
       end
     end
   end
-
 end
 
-if __FILE__ == $PROGRAM_NAME
-  main
+autoingestor = Autoingestion.new
+yesterday = (Time.new - 24*60*60).strftime("%Y%m%d")
+
+if File.exists?("config.hash")
+  config = eval(File.new("config.hash").read())
+  autoingestor.username = config["username"]
+  autoingestor.password = config["password"]
+  autoingestor.vendor_number = config["vendor_number"]
+else
+  print "iTunes Connect email address:             "
+  autoingestor.username = gets.strip!
+  print "iTunes Connect password:                  "
+  autoingestor.password = gets.strip!
+  print "Vendor number:                            "
+  autoingestor.vendor_number = gets.strip!
+  print "Type of report:                [Sales]    "
+  autoingestor.type_of_report = gets.strip!
+  print "Date type:                     [Daily]    "
+  autoingestor.date_type = gets.strip!
+  print "Report type:                   [Summary]  "
+  autoingestor.report_type = gets.strip!
+  print "Report date:                   [#{yesterday}] "
+  autoingestor.report_date = gets.strip!
 end
+
+autoingestor.type_of_report = "Sales"   if autoingestor.type_of_report.nil?
+autoingestor.date_type      = "Daily"   if autoingestor.date_type.nil?
+autoingestor.report_type    = "Summary" if autoingestor.report_type.nil?
+autoingestor.report_date    = yesterday if autoingestor.report_date.nil?
+autoingestor.perform_request
